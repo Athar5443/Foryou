8f8f4cd0fb95f506c2f97f8506dd803c5772266e
Athar5443 <azkaathar5443@gmail.com>
2025-01-29 14:26:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,36 @@
-var c = document.getElementById('alx');
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-  var b = document.body;
-  var a = c.getContext('2d');
+// Sesuaikan ukuran canvas dengan ukuran jendela
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+// Fungsi untuk menggambar hati
+function drawHeart(x, y, size) {
+  ctx.beginPath();
+  ctx.moveTo(x, y - size * 0.5);
+  ctx.bezierCurveTo(x - size * 0.2, y - size * 0.7, x - size * 0.6, y + size * 0.2, x, y + size * 0.5);
+  ctx.bezierCurveTo(x + size * 0.6, y + size * 0.2, x + size * 0.2, y - size * 0.7, x, y - size * 0.5);
+  ctx.fillStyle = 'red';
+  ctx.fill();
+}
 
-  e = [];
-  h = [];
-  WIDTH = c.width = innerWidth;
-  HEIGHT = c.height = innerHeight;
-  v = 32 + 16 + 8;
-  R = Math.random;
-  C = Math.cos;
-  Y = 6.3;
+// Fungsi untuk menulis teks
+function writeText(text, x, y, fontSize) {
+  ctx.font = `${fontSize}px sans-serif`;
+  ctx.fillStyle = 'white';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillText(text, x, y);
+}
 
-  for (i = 0; i < Y ; i += 0.2) 
-    h.push([WIDTH / 2  + 210 * Math.pow(Math.sin(i), 3), 
-      HEIGHT / 2  + 13 * -(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i))]);
+// Gambar hati di tengah canvas
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+const heartSize = 200;
+drawHeart(centerX, centerY, heartSize);
 
-  for (i = 0; i < Y  ; i += 0.4) 
-    h.push([WIDTH / 2  + 150 * Math.pow(Math.sin(i), 3), 
-      HEIGHT / 2  + 9 * -(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i))]);
-
-  for (i = 0; i < Y  ; i += 0.8) 
-    h.push([WIDTH / 2  + 90 * Math.pow(Math.sin(i), 3), 
-      HEIGHT / 2  + 5 * -(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i))]);
-
-  for (i = 0; i < v;) {
-      x = R() * WIDTH;
-      y = R() * HEIGHT;
-      H = 80 * (i / v) + Math.random * 100;
-      S = 40 * R() + 60;
-      B = 60 * R() + 20;
-      f = [];
-      for (k = 0; k < v;) f[k++] = {
-          x: x,
-          y: y,
-          X: 0,
-          Y: 0,
-          R: 1 - k / v + 1,
-          S: R() + 1,
-          q: ~~(R() * v),
-          D: 2 * (i % 2) - 1,
-          F: 0.2 * R() + 0.7,
-          f: "hsla(" + ~~H + "," + ~~S + "%," + ~~B + "%,.1)"
-      };
-      e[i++] = f
-  }
-
-  function path(d) {
-      a.fillStyle = d.f;
-      a.beginPath();
-      a.arc(d.x, d.y, d.R, 0, Y, 1);
-      a.closePath();
-      a.fill()
-  }
-  setInterval(function () {
-      a.fillStyle = "rgba(0,0,0,.2)";
-      a.fillRect(0, 0, WIDTH, HEIGHT);
-      for (i = v; i--;) {
-          f = e[i];
-          u = f[0];
-          q = h[u.q];
-          D = u.x - q[0];
-          E = u.y - q[1];
-          G = Math.sqrt(D * D + E * E);
-          10 > G && (0.95 < R() ? u.q = ~~ (R() * v) : (0.99 < R() && (u.D *= -1), u.q += u.D, u.q %= v, 0 > u.q && (u.q += v)));
-          u.X += -D / G * u.S;
-          u.Y += -E / G * u.S;
-          u.x += u.X;
-          u.y += u.Y;
-          path(u);
-          u.X *= u.F;
-          u.Y *= u.F;
-          for (k = 0; k < v - 1;) T = f[k], N = f[++k], N.x -= 0.7 * (N.x - T.x), N.y -= 0.7 * (N.y - T.y), path(N)
-      }
-  }, 25);
+// Tulis teks "For Lia" di tengah hati
+const text = 'For Lia';
+const textSize = 40;
+writeText(text, centerX, centerY, textSize);
